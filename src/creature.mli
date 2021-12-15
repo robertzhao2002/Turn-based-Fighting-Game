b00f4a4fe8c1053b450788b8a830b3150a26abda
Robert Zhao <rzhao1234@gmail.com>
2021-12-15 01:42:51 -0500

    Organize json folders and add documentation for move and creature

--- src/creature.mli
+++ src/creature.mli
@@ -1,2 +1,20 @@
 type t
 (** The abstract type representing a creature that can be used. *)
+
+val name : t -> string
+(** [name c] is the name of creature [c]. *)
+
+val hp : t -> int
+(** [hp c] is the base hp of creature [c]. This is the amount of hitpoints [c] has. *)
+
+val attack : t -> int
+(** [attack c] is the base attack of creature [c]. This determines how much damage [c] can deal
+    when using a given move. *)
+
+val defense : t -> int
+(** [defense c] is the base defense of creature [c]. This determines how easy/hard it is for
+    [c] to lose hp. *)
+
+val speed : t -> int
+(** [speed c] is the base speed of creature [c]. This determines who goes first in a given
+    turn. *)
--- src/move.mli
+++ src/move.mli
@@ -1,2 +1,22 @@
-type t
-(** The abstract type representing a move that can be used. *)
+type t =
+  | Damaging
+  | Status
+
+val name : t -> string
+(** [name m] is the name of move [m]. *)
+
+val power : t -> int
+(** [power m] is the base power of move [m]. This determines how much damage [m] can
+    potentially do. Moves with higher base power will do more damage when used by the same
+    creature. *)
+
+val accuracy : t -> int
+(** [accuracy m] is the base accuracy of move [m]. This determines how likely the move will hit
+    the target. If it hits the target, it will do damage based on the base power and base
+    attack stat of the creature. If it does not hit (misses), then 0 damage is done. *)
+
+val uses : t -> int
+(** [uses m] is the number of times move [m] can be used in a battle. Each move has a
+    predetermined number of uses. Every time u use [m], this number decrements. Whenever
+    [uses m] is 0, then [m] cannot be used anymore. When a creature has no more moves to use,
+    it automatically dies, even if it is at full hp. *)
