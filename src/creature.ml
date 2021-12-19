f04a29fcdae67633585c6bfb4feb59129682787c
Robert Zhao <rzhao1234@gmail.com>
2021-12-18 19:24:37 -0500

    Change schema and implement basic property functions

--- src/creature.ml
+++ src/creature.ml
@@ -1,1 +1,23 @@
-type t = string
+type status =
+  | Poison
+  | Confuse
+  | Paralyze
+
+type t = {
+  name : string;
+  hp : int;
+  attack : int;
+  defense : int;
+  speed : int;
+  status : status;
+}
+
+let name c = c.name
+
+let hp c = c.hp
+
+let attack c = c.attack
+
+let defense c = c.defense
+
+let speed c = c.speed
