ce2b50a600ac7057e9a3b5ac07bf2a6743084119
Robert Zhao <rzhao1234@gmail.com>
2021-12-21 22:38:31 -0500

    Revive gives half hp, everything else same as original

--- src/trainer.ml
+++ src/trainer.ml
@@ -24,14 +24,35 @@ let revive (trainer : t) (creature : Creature.t) =
       let c3name = trainer.creature3.name in
       if revived_name = c1name then
         if dead trainer.creature1 then
-          { trainer with creature1 = { trainer.creature1 with hp = hp trainer.creature1 / 2 } }
+          {
+            trainer with
+            creature1 =
+              {
+                (init_creature_with_name trainer.creature1.name) with
+                hp = hp trainer.creature1 / 2;
+              };
+          }
         else raise (CreatureNotDead trainer.creature1)
       else if revived_name = c2name then
         if dead trainer.creature2 then
-          { trainer with creature2 = { trainer.creature2 with hp = hp trainer.creature2 / 2 } }
+          {
+            trainer with
+            creature2 =
+              {
+                (init_creature_with_name trainer.creature2.name) with
+                hp = hp trainer.creature2 / 2;
+              };
+          }
         else raise (CreatureNotDead trainer.creature1)
       else if revived_name = c3name then
         if dead trainer.creature3 then
-          { trainer with creature3 = { trainer.creature3 with hp = hp trainer.creature3 / 2 } }
+          {
+            trainer with
+            creature3 =
+              {
+                (init_creature_with_name trainer.creature3.name) with
+                hp = hp trainer.creature3 / 2;
+              };
+          }
         else raise (CreatureNotDead trainer.creature1)
       else raise NoCreaturesDead
