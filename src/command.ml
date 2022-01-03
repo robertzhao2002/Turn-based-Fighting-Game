321221c0f47c43eaa1d9a00d651af28d13ebf72d
Robert Zhao <rzhao1234@gmail.com>
2022-01-02 22:09:27 -0500

    Finished command parsing module

--- src/command.ml
+++ src/command.ml
@@ -15,10 +15,30 @@ let format str = str |> String.trim |> String.lowercase_ascii
 
 let rec get_words_no_spaces str_lst = List.filter (( <> ) "") str_lst
 
+let first_word = function
+  | [] -> raise Malformed
+  | h :: _ -> h
+
+let rest_words = function
+  | [] -> raise Malformed
+  | _ :: t -> t
+
 let parse s =
   let formatted = format s in
-  let words_list = String.split_on_char ' ' formatted in
+  let words_list = String.split_on_char ' ' formatted |> get_words_no_spaces in
   match formatted with
   | "quit" -> Quit
   | "surrender" -> Surrender
-  | _ -> raise Malformed
+  | _ -> begin
+      let fst_word = first_word words_list in
+      let rest = rest_words words_list in
+      if rest = [] then raise Malformed
+      else
+        match fst_word with
+        | "summary" -> Summary (String.concat " " rest)
+        | "info" -> Info (String.concat " " rest)
+        | "use" -> UseMove (String.concat " " rest)
+        | "switch" -> Switch (String.concat " " rest)
+        | "revive" -> Revive (String.concat " " rest)
+        | _ -> raise Malformed
+    end
--- test/command_tests.ml
+++ test/command_tests.ml
@@ -4,12 +4,34 @@ open Game.Command
 let parse_test name input expected_output =
   name >:: fun _ -> parse input |> assert_equal expected_output
 
+let parse_excep_test name str exn = name >:: fun _ -> assert_raises exn (fun () -> parse str)
+
 let parse_tests =
   [
     parse_test "    qUiT    will parse to Quit" "    qUiT   " Quit;
     parse_test "suRRenDer    will parse to Quit" "suRRenDer   " Surrender;
+    parse_test "    sUmMary       CREATURE" "    sUmMary       CREATURE" (Summary "creature");
+    parse_test "info this     attack" "info this     attack" (Info "this attack");
+    parse_test "use       attack" "use       attack" (UseMove "attack");
+    parse_test "switch creature2" "switch creature2" (Switch "creature2");
+    parse_test "revive this creature now" "revive this creature now"
+      (Revive "this creature now");
   ]
 
-let suite = "test suite for Command module" >::: List.flatten [ parse_tests ]
+let parse_excep_tests =
+  [
+    parse_excep_test "       " "       " Malformed;
+    parse_excep_test "sdfgkljsdfgklsdjfnglksdjfng" "sdfgkljsdfgklsdjfnglksdjfng" Malformed;
+    parse_excep_test "info" "info" Malformed;
+    parse_excep_test "use" "use" Malformed;
+    parse_excep_test "revive" "revive" Malformed;
+    parse_excep_test "switch" "switch" Malformed;
+    parse_excep_test "    summary   " "    summary   " Malformed;
+    parse_excep_test "quitsbdfbsddf" "quitsbdfbsddf" Malformed;
+    parse_excep_test "surrender sbdfbsddf" "surrender sbdfbsddf" Malformed;
+  ]
+
+let suite =
+  "test suite for Command module" >::: List.flatten [ parse_tests; parse_excep_tests ]
 
 let _ = run_test_tt_main suite
