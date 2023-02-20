b1a04f9972b793df3867c27fafe5966bbebd4420
Robert Zhao <44549096+robertzhao2002@users.noreply.github.com>
2023-02-19 21:14:50 -0500

    Add CI (#3)

--- test/trainer_tests.ml
+++ test/trainer_tests.ml
@@ -6,8 +6,17 @@ open Game.Trainer
 let trainer_string_test name input expected_output =
   name >:: fun _ -> trainer_string input |> assert_equal expected_output ~printer:id
 
-let trainer1_tests = [ trainer_string_test "trainer1 string" trainer1 "" ]
+let trainer1_tests =
+  [
+    trainer_string_test "trainer1 string" trainer1
+      {|Trainer1
+REVIVE
+Jit (type5/type2/type1): 100.0% HP; CONFUSE; (IN BATTLE)
+Jit (type5/type2/type1): 100.0% HP;
+Jit (type5/type2/type1): 95.0% HP; PSN;
+Jit's Moves
+- yell (type1): 2/2 uses|};
+  ]
 
 let suite = "test suite for Trainer module" >::: List.flatten [ trainer1_tests ]
-
 let _ = run_test_tt_main suite
--- test/values.ml
+++ test/values.ml
@@ -5,31 +5,22 @@ open Helper
 
 (* Moves *)
 let yell = init_move_with_name "yell"
-
 let yell_2 = use_times yell 2
-
 let nutty = init_move_with_name "nutty"
-
 let nutty_5 = use_times nutty 5
-
 let chug_jug = init_move_with_name "chug jug"
 
 (* Creatures *)
 let jit_test = init_creature_with_name "Jit"
-
 let jit_attack_reduced = { jit_test with attack = 0.5 *. jit_test.attack }
 
 let jit_defense_boost_attack_reduced =
   { jit_attack_reduced with defense = 1.5 *. jit_attack_reduced.defense }
 
 let jit_poison = { jit_test with poison = true }
-
 let jit_after_poison = { jit_poison with hp = jit_poison.hp *. 0.95 }
-
-let jit_confuse = { jit_test with confuse = Some 0 }
-
-let jit_confuse_1_turn = { jit_confuse with hp = jit_confuse.hp *. 0.9; confuse = Some 1 }
-
+let jit_confuse = { jit_test with confuse = Some 1 }
+let jit_confuse_1_turn = { jit_confuse with hp = jit_confuse.hp *. 0.9; confuse = Some 2 }
 let jit_everything = jit_test
 
 (* Trainers *)
